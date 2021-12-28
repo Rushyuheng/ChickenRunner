51c2e50499af83e3564a01930a86a672eea06d65
LoeiKimi <95698966+LoeiKimi@users.noreply.github.com>
2021-12-28 09:42:44 +0800

    Player and Camera Control

---
+++ ChickenRunner/Assets/Script/Player Control/PlayerControl2.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl2 : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float forwardSpeed, reverseSpeed, turnSpeed;
+    private float moveInput, turnInput;
+    void Start()
+    {
+        rb.transform.parent = null;
+    }
+
+    void Update()
+    {
+        //forward and backward movement
+        moveInput = Input.GetAxis("Vertical2");
+        moveInput *= moveInput > 0 ? forwardSpeed : reverseSpeed;
+
+        //left and right movement
+        turnInput = Input.GetAxis("Horizontal2");
+        float newRotation = turnInput * turnSpeed * Time.deltaTime * Input.GetAxis("Vertical2");
+        transform.Rotate(0, newRotation, 0, Space.World);
+
+
+        //set the sphere with the car
+        transform.position = rb.transform.position;
+    }
+    private void FixedUpdate()
+    {
+        rb.AddForce(transform.forward * moveInput, ForceMode.Acceleration);
+    }
+}
